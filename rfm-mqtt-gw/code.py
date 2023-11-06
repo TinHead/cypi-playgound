3e9a9f3a1b005e4ca53641a7893ae3e6e911a2da
TinHead <razlix77@gmail.com>
2023-11-06 21:02:22 +0200

    gw ready

--- rfm-mqtt-gw/code.py
+++ rfm-mqtt-gw/code.py
@@ -19,6 +19,7 @@ print(f"Connecting to {os.getenv('CIRCUITPY_WIFI_SSID')}")
 wifi.radio.connect(
     os.getenv("CIRCUITPY_WIFI_SSID"), os.getenv("CIRCUITPY_WIFI_PASSWORD")
 )
+wifi.radio.hostname = "RfGw"
 
 print(f"Connected to {os.getenv('CIRCUITPY_WIFI_SSID')}!")
 print(f"My IP address: {wifi.radio.ipv4_address}")
